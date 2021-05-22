4c4c61ea0114233c9e0faf31433a3655da253fff
Tim Groenewoud <74865860+TimGroenewoud@users.noreply.github.com>
2021-05-22 15:12:16 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+// Function Arguments + Multiple Arguments
+
+function paintWalls(walls, colors) {
+    console.log('The wall is painted ' + colors);
+    console.log('the highest wall is painted ' + walls);
+}
+
+
+
+paintWalls('orange', 'red');
